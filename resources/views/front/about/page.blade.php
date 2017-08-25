606d6aef167dcc2341f100571f9b53a81fcf8614
Michael Joyner <joyner.michael@gmail.com>
2017-08-25 09:00:55 +0800

    about page

--- resources/views/front/about/page.blade.php
+++ resources/views/front/about/page.blade.php
@@ -17,9 +17,15 @@
         <h1 class="h1 uppercase text-colour text-center">@lang('about.intro.heading')</h1>
     </div>
     <div class="w-con-800 pd-x-4 mg-y-10 mg-x-a">
-        <p class="body-text text-constrain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, ipsum, ratione! Aperiam assumenda distinctio dolorem, esse ex expedita harum laboriosam mollitia neque odit perspiciatis porro quibusdam quidem, repudiandae sed tempore.</p>
-        <p class="body-text text-constrain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, ipsum, ratione! Aperiam assumenda distinctio dolorem, esse ex expedita harum laboriosam mollitia neque odit perspiciatis porro quibusdam quidem, repudiandae sed tempore.</p>
-        <p class="body-text text-constrain">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, ipsum, ratione! Aperiam assumenda distinctio dolorem, esse ex expedita harum laboriosam mollitia neque odit perspiciatis porro quibusdam quidem, repudiandae sed tempore.</p>
+        <p class="body-text text-constrain text-largish text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, ipsum, ratione! Aperiam assumenda distinctio dolorem, esse ex expedita harum laboriosam mollitia neque odit perspiciatis porro quibusdam quidem, repudiandae sed tempore.</p>
+    </div>
+    <div class="flex flex-split w-con-800 mb-10 mg-x-a">
+        <div>
+            <img class="w-con-300 w-100 block mg-x-a" src="/images/ben.jpg" alt="Ben, co-founder of Uptowner">
+        </div>
+        <div>
+            <img class="w-con-300 w-100 block mg-x-a" src="/images/kyle.jpg" alt="Kyle, co-founder of Uptowner">
+        </div>
     </div>
 
 @endsection
