550adb5cd02e20fd01e4d3eae6232aec91d267ee
Michael Joyner <joyner.michael@gmail.com>
2018-01-19 08:42:04 +0800

    update events page copy

--- resources/lang/en/events.php
+++ resources/lang/en/events.php
@@ -7,7 +7,7 @@ return [
     ],
     'intro' => [
         'heading' => 'Live Sports and Events',
-        'body' => 'The Uptowner is one of the few places in Taichung that shows live sports from North America, including NFL, NBA and NHL. Catch the game on one of our 12 plasma TV’s while munching on some snacks and drinking a cold beverage. We even open especially for big games!'
+        'body' => 'The Uptowner can show NBA, NHL, MLB, NFL, UFC, F1 and many more sporting events live or on demand, just ask! We have four large TVs throughout the restaurant and bar for convenient viewing. Reservations may be required for larger events, so give us a call or stop in first to make sure you get a good seat!'
     ],
     'coming_soon' => [
         'heading' => 'Coming Soon'
